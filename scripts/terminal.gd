9a62867aeddbc0ffc656f4b8b58330e49133fa46
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-01 17:54:34 +1200

    terminal stuff

--- scripts/terminal.gd
+++ scripts/terminal.gd
@@ -1,10 +1,28 @@
 extends TextEdit
 
 
+var last_caret_column: int
+
+
 func _on_text_changed():
 	if not text.begins_with(" "):
-		text.insert(0, " ")
-
+		text = " " + text
+	
+	if len(text) <= 1:
+		text = ""
+		return
+	
+	if len(text) == 2:
+		set_caret_column(2)
+	
+	if text.contains("\n"):
+		text = text.replace("\n", "")
+		set_caret_column(last_caret_column)
+	
+	last_caret_column = get_caret_column()
 
 func _on_caret_changed():
-	get_caret_column()
+	if get_caret_column() < 1:
+		set_caret_column(1)
+	
+	last_caret_column = get_caret_column()
