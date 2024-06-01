4f9c9eb156aee7b73a48b520f49d753642395578
Boop <102631339+BoopEnthusiast@users.noreply.github.com>
2024-06-01 14:23:53 +1200

    minor project settings changed + pathfinding

--- scripts/npc_navigation.gd
+++ scripts/npc_navigation.gd
@@ -2,8 +2,8 @@ class_name NPCNavigation extends NavigationAgent2D
 
 
 # Constants
-const PATH_RADIUS = 50.0
+const PATH_RADIUS = 300.0
 
 
 func find_new_path() -> void:
-	target_position = get_parent().global_position + Vector2(randf() - 0.5, randf() - 0.5).normalized() * PATH_RADIUS
+	target_position = get_parent().global_position + Vector2(randf() - 0.5, randf() - 0.5).normalized() * PATH_RADIUS * clamp(randf(), 0.5, 1)
