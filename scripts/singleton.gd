b1c9f6eb32e7b01f8c1e0303b3b9f3edfa6eff42
Paselle <felixcabage@gmail.com>
2024-06-01 17:42:42 +1200

    added hunger, thirst, and cold
    added hunger, thirst, and cold as variables in singleton.gd

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -1,3 +1,9 @@
 extends Node
 
 
+var remaining_days = 50
+
+
+func end_day():
+	for villager: Villager in get_tree().get_nodes_in_group("villager"):
+		villager.day_pass(randi_range(1, 10), randi_range(1, 10), randi_range(1, 10))
