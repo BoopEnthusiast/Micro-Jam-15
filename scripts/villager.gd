d14b99f38a3f45fe2b27e0b5d1b6cdf511a6c4ec
Kade1O1 <forsyth.kade@gmail.com>
2024-06-01 16:49:25 +1200

    added day_pass function

--- scripts/villager.gd
+++ scripts/villager.gd
@@ -1,11 +1,52 @@
 class_name Villager extends NPC
 
 # Survival variables
-var food = 10
-var water = 10
-var warmth = 10
-var health = 10
+var food = 100
+var water = 100
+var warmth = 100
+
+var total_health = 100
+var health = 100
+
+var sickness = 0
+
+# Booleans
+var is_hungry = false
+var is_thirsty = false
+var is_cold = false
 
 # functions
 func death():
 	pass
+
+func day_pass(hunger, thirst, cold):
+
+		
+	food -= hunger
+	water -= thirst
+	warmth -= cold
+	
+	if food < 20 and !is_hungry:
+		is_hungry = true
+		total_health -= 25
+	else :
+		is_hungry = false
+		total_health += 25
+	
+	if water < 20 and !is_thirsty:
+		is_thirsty = true
+		total_health -= 25
+	else :
+		is_thirsty = false
+		total_health += 25
+	
+	if warmth < 20  and !is_cold:
+		is_cold = true
+		total_health -= 25
+	
+	if total_health > 100 :
+		total_health = 100
+	
+	if food <= 0 or water <= 0 or warmth <= 0 or health <= 0:
+		death()
+	
