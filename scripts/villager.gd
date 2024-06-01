dcf395f8c7d0bd7b27e26f0a80bb386c9490fef8
Kade1O1 <forsyth.kade@gmail.com>
2024-06-01 19:21:15 +1200

    added sickness

--- scripts/villager.gd
+++ scripts/villager.gd
@@ -19,6 +19,13 @@ var is_cold = false
 func death():
 	pass
 
+func get_sick(days) :
+	var sick_chance = 400 - food - water - warmth - health
+	if randi_range(0, 300) < sick_chance :
+		sickness += 2
+	
+	sickness += days
+
 func day_pass(hunger, thirst, cold):
 
 	# Update survival variables
@@ -48,12 +55,21 @@ func day_pass(hunger, thirst, cold):
 		is_cold = false
 		total_health += 25
 	
+	# Check if villager is sick
+	if sickness > 0 :
+		health -= 5
+	else :
+		health += 5
+		get_sick(0)
+	
+	
 	# Update villagers health
 	if total_health > 100 :
 		total_health = 100
 	
 	if health > total_health :
 		health = total_health
+	
 	
 	# Kill villager if any survival variable are 0
 	if food <= 0 or water <= 0 or warmth <= 0 or health <= 0:
