2583038bc8d1f1ec7f3266300abe86b3937cf5d0
Kade1O1 <forsyth.kade@gmail.com>
2024-06-02 20:23:21 +1200

    Update singleton.gd

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -21,6 +21,9 @@ func end_day():
 	ghosts = get_tree().get_nodes_in_group("ghost") as Array[Ghost]
 	var need_resource: int
 	
+	if villagers.size() <= 0:
+		pass # end game lose
+	
 	# evenly distribute resources amonst villagers
 	while food > 0 and need_resource > 0:
 		need_resource = len(villagers)
@@ -57,5 +60,23 @@ func end_day():
 	# update villagers at the end of the day
 	for villager: Villager in villagers:
 		villager.day_pass(randi_range(1, 10), randi_range(1, 10), randi_range(1, 10))
+	
+	if remaining_days > 0:
+		remaining_days -= 1
+		if remaining_days % 10 == 9:
+			random_calamity()
+		if remaining_days != 50 and remaining_days % 10 == 0:
+			play_calamity()
+	else:
+		pass # end game win
 
 
+func random_calamity():
+	pass
+	#Blizzard - water become frozen for x days, all crops die
+	#Plague - random numkber of villagers get sick for x days
+	#Drought - all crops die, all water stores are removed
+	#Thunderstorm - random number of trees are removed
+
+func play_calamity():
+	pass
