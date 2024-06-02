506e797bb962009cc098874c23283ead91bc4939
Paselle <felixcabage@gmail.com>
2024-06-02 14:03:26 +1200

    Started work on resources and added resource variables

--- scripts/singleton.gd
+++ scripts/singleton.gd
@@ -2,8 +2,13 @@ extends Node
 
 
 var remaining_days = 50
+var wood
+var food
+var water
 
 
 func end_day():
 	for villager: Villager in get_tree().get_nodes_in_group("villager"):
 		villager.day_pass(randi_range(1, 10), randi_range(1, 10), randi_range(1, 10))
+
+
