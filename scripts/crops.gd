1ac168a01e15ccc675970b8198965637b4ade8fa
Paselle <felixcabage@gmail.com>
2024-06-02 16:37:45 +1200

    added some functionality for generating resources

--- scripts/crops.gd
+++ scripts/crops.gd
@@ -2,3 +2,8 @@ class_name Crops extends Resources
 
 
 var crops = 5
+
+
+func harvest_crops():
+	crops -= 1
+	generate_resource(5)
--- scripts/forest.gd
+++ scripts/forest.gd
@@ -2,3 +2,8 @@ class_name Forest extends Resources
 
 
 var trees = 5
+
+
+func chop_tree():
+	trees -= 1
+	generate_resource(5)
